33a0cc578cc858562c589fb05e72b43e2efca5e5
illinia <seolin1218@gmail.com>
2021-10-04 23:05:03 +0900

    deploy ropsten network

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,8 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync('.secret').toString().trim();
+const infuraKey = '7c77ddd7f17f4ce18129493d73be7070';
+
 module.exports = {
   networks: {
     development: {
@@ -10,9 +15,16 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*'
+    },
+    ropsten: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic,
+          `https://ropsten.infura.io/v3/${infuraKey}`)
+      },
+      network_id: 3,
+      gass: 4700000
     }
   },
-  mocha: {},
   compilers: {
     solc: {
       version: "0.8.0"
